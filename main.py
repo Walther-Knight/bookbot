06dd547e1ee7ab944b7a2b1a970630954b87ad29
walther_knight <knightofargh@gmail.com>
2025-01-31 14:27:24 -0500

    update main.py to use read_book function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 
-def main():
-    path_to_file = "books/frankenstein.txt"
+def read_book(path_to_file):
     try:
         with open(path_to_file, "r") as f:
             file_contents = f.read()
@@ -9,4 +8,8 @@ def main():
     except:
         print("File Not Found!")
 
-main()
+def main():
+    read_book("books/frankenstein.txt")
+
+if __name__ == "__main__":
+    main()
