e2aada6aa5ff549b328612147b9530764680d684
walther_knight <knightofargh@gmail.com>
2025-01-31 16:15:05 -0500

    updated main.py for formatting

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    word_list = read_book(book_path)
+    word_count(word_list)
+
+    
+
+''' main with input option
+def main():
+    file_path = f"books/{input('Enter the file name: ')}"
+    read_book(file_path)
+'''
 
 #function to read file and print to stdout
 def read_book(path_to_file):
     try:
         with open(path_to_file, "r") as f:
             file_contents = f.read()
-            #print(file_contents)
+            print(file_contents)
         return file_contents
     except:
         print("File Not Found!")
@@ -13,19 +25,9 @@ def read_book(path_to_file):
 def word_count(word_list):
     try:
         words = len(word_list.split())
-        print(words)
+        print(f"Word Count: {words}")
     except:
         print("File Not Found, Cannot Count Words!")
-
-def main():
-    word_list = read_book("books/frankenstein.txt")
-    word_count(word_list)
-
-''' main with input option
-def main():
-    file_path = f"books/{input('Enter the file name: ')}"
-    read_book(file_path)
-'''
 
 if __name__ == "__main__":
     main()
