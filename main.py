d9df50eaacebd0891f4c98be4b50fddb09621993
walther_knight <knightofargh@gmail.com>
2025-01-31 16:05:35 -0500

    add word_count()

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 
-
+#function to read file and print to stdout
 def read_book(path_to_file):
     try:
         with open(path_to_file, "r") as f:
             file_contents = f.read()
-        print(file_contents)
+            #print(file_contents)
+        return file_contents
     except:
         print("File Not Found!")
+    
+#function to return word count
+def word_count(word_list):
+    try:
+        words = len(word_list.split())
+        print(words)
+    except:
+        print("File Not Found, Cannot Count Words!")
 
 def main():
-    read_book("books/frankenstein.txt")
+    word_list = read_book("books/frankenstein.txt")
+    word_count(word_list)
 
 ''' main with input option
 def main():
