0bbc33623bf997087f8bf328550b29ca6e7269bb
walther_knight <knightofargh@gmail.com>
2025-02-01 13:14:31 -0500

    completed report function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
-    book_path = "books/test.txt"
+    book_path = "books/frankenstein.txt"
     word_list = read_book(book_path)
     if word_list:
         print(word_list)
-        word_count(word_list)
+        number_of_words = word_count(word_list)
         characters = character_count(word_list)
-        print(f"Number of Characters: {characters}")
-        print(character_report(characters))
+        character_report(characters, book_path, number_of_words)
+        
     else:
         print("File Not Found!")
     
@@ -28,33 +28,30 @@ def read_book(path_to_file):
     
 #function to return word count
 def word_count(words):
-    print(f"Word Count: {len(words.split())}")
+    return len(words.split())
 
 #function to return character count
 def character_count(words):
+    # define allowed characters for report
+    allowed_character = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",}
     num_char = {}
     working_file = list(words.lower())
     for i in working_file:
-        if i in num_char:
-            num_char[i] +=1
-        else:
-            num_char[i] = 1
+        if i in allowed_character:
+            if i in num_char:
+                num_char[i] +=1
+            else:
+                num_char[i] = 1
     return num_char
     
 # function to report on character counts, no special characters
-def character_report(characters):
-    # define allowed characters for report
-    allowed_character = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
-    character_dict = {}
+def character_report(characters, book_path, number_of_words):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{number_of_words} words found in the document")
+    print("")
     for character in characters:
-        if character in allowed_character:
-            if character in character_dict:
-                character_dict[character] += 1
-            else:
-                character_dict[character] = 1
-        elif character not in allowed_character:
-            pass
-    return character_dict
+        print(f"The '{character}' character was found {characters[character]} times")
+    print("--- End Report ---")
 
 
 if __name__ == "__main__":
